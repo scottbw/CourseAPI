ac898ae26f0d6288c776f744d48f90f3a587ae14
scottbw <scott.bradley.wilson@gmail.com>
2016-04-06 14:26:50 +0100

    extended models

--- CourseAPI/Models/Course.cs
+++ CourseAPI/Models/Course.cs
@@ -7,12 +7,25 @@ namespace CourseAPI.Models
 {
     public class Course
     {
-       public  String areaOfStudyCode;
-       public  String academicYear;
-       public  String occurrenceCode;
+        public String areaOfStudyCode;
+        public String academicYear;
+        public String occurrenceCode;
         public String customisedFor;
         public String version;
         public String areaOfStudyTypeCode;
+        public String parentAreaOfStudyCode;
+        public String name;
+        public String description;
+        public String category;
+        public int credits = 0;
+        public String qualificationAim;
+        public String facultyCode;
+        public String departmentCode;
+        public Date startDate;
+        public Date endDate;
+        public int staffId = 0;
+        public String attendanceMode;
+        public String jacs;
 
         public Course(String aos_code, String acad_year, String occurrence)
         {
---
+++ CourseAPI/Models/CourseForStudent.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CourseAPI.Models
+{
+    public class CourseForStudent extends Course
+    {
+
+        public String enrolmentStageCode;
+        public String enrolmentStatusCode
+        public String enrolmentTypeCode
+        public String feeStatusCode
+
+    }
+}
