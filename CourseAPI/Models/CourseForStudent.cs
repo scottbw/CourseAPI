1b78cf1373a36d3e61c02209c83a1bd38de7eb2b
scottbw <scott.bradley.wilson@gmail.com>
2016-04-06 14:42:04 +0100

    added semicolons

--- CourseAPI/Models/CourseForStudent.cs
+++ CourseAPI/Models/CourseForStudent.cs
@@ -7,18 +7,17 @@ namespace CourseAPI.Models
 {
     public class CourseForStudent : Course
     {
-        public String personalisedFor;
         public String enrolmentStageCode;
-        public String enrolmentStatusCode
-        public String enrolmentTypeCode
-        public String feeStatusCode
+        public String enrolmentStatusCode;
+        public String enrolmentTypeCode;
+        public String feeStatusCode;
 
         public CourseForStudent(String aos_code, String acad_year, String occurrence, String student)
         {
             this.areaOfStudyCode = aos_code;
             this.academicYear = acad_year;
             this.occurrenceCode = occurrence;
-            this.customisedFor = student;
+            this.personalisedFor = student;
             this.version = "Current";
             this.areaOfStudyTypeCode = "C";
         }
